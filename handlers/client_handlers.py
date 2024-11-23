b422855b80ec799ce5f9b073c10a2f7e0bc6eca5
Simeon Kolchin <62173656+simeonkolchin@users.noreply.github.com>
2024-11-23 21:17:44 +0300

    edit user data

--- handlers/client_handlers.py
+++ handlers/client_handlers.py
@@ -14,6 +14,8 @@ class client_state(StatesGroup):
     get_client_name = State()
     get_client_tel = State()
 
+    user_await = State()
+
     edit_client_name = State()
     edit_client_tel = State()
 
@@ -30,6 +32,7 @@ async def start(message: Message, state: FSMContext):
         await state.set_state(client_state.get_client_name)
 
 
+# REGISTRATION
 @router.message(client_state.get_client_name)
 async def get_client_name(message: Message, state: FSMContext):
     client_name = message.text
@@ -40,7 +43,8 @@ async def get_client_name(message: Message, state: FSMContext):
         await state.set_state(client_state.get_client_tel)
     else:
         await message.answer(
-            f"К сожалению, этих данных недостаточно( Проверьте наличие фамилии или имени и отправьте мне ФИО повторно:")
+            f"К сожалению, этих данных недостаточно( "
+            f"Проверьте наличие фамилии или имени и отправьте мне ФИО повторно:")
         await state.set_state(client_state.get_client_name)
 
 
@@ -48,23 +52,25 @@ async def get_client_name(message: Message, state: FSMContext):
 async def get_client_tel(message: Message, state: FSMContext):
     client_tel = message.text
     await state.update_data(client_tel=client_tel)
+    await state.set_state(client_state.user_await)
     user_data = await state.get_data()
     await message.answer(f"Ваши данные: \n "
                          f"Имя: {user_data['client_name']}\n "
                          f"Телефон: {user_data['client_tel']}",
                          reply_markup=keyboards.get_client_tel_keyboard().as_markup())
-    await state.set_state(client_state.get_client_tel)
 
 
-@router.callback_query(F.data == "client_data_save")
-async def client_data_save_callback(callback: types.CallbackQuery, state: FSMContext):
+@router.message(F.data == "user_await")
+async def user_await(message: Message, state: FSMContext):
     user_data = await state.get_data()
-    user_manager = User()
-    await user_manager.create_user(callback.message.chat.id, user_data["client_name"], user_data["client_tel"])
-    await callback.message.edit_text(f"Данные сохранены. Меню ")
-    await state.clear()
+    await message.answer(f"Ваши данные: \n "
+                         f"Имя: {user_data['client_name']}\n "
+                         f"Телефон: {user_data['client_tel']}",
+                         reply_markup=keyboards.get_client_tel_keyboard().as_markup())
+    await state.set_state(client_state.user_await)
 
 
+# EDIT USER
 @router.callback_query(F.data == "client_data_edit")
 async def client_data_edit_callback(callback: types.CallbackQuery, state: FSMContext):
     user_data = await state.get_data()
@@ -72,7 +78,52 @@ async def client_data_edit_callback(callback: types.CallbackQuery, state: FSMCon
                                      reply_markup=keyboards.client_data_edit_keyboard(user_data["client_name"],
                                                                                       user_data["client_tel"]).as_markup())
 
-#
-# @router.callback_query(F.data == "client_data_edit_name")
-# async def client_data_edit_name_callback(callback: types.CallbackQuery, state: FSMContext):
-#
+@router.callback_query(F.data == "client_data_edit_name")
+async def client_data_edit_name_callback(callback: types.CallbackQuery, state: FSMContext):
+    await callback.message.edit_text(f"Отправь мне заново свое ФИО:")
+    await state.set_state(client_state.edit_client_name)
+
+
+@router.callback_query(F.data == "client_data_edit_tel")
+async def client_data_edit_name_callback(callback: types.CallbackQuery, state: FSMContext):
+    await callback.message.edit_text(f"Отправь мне заново свой номер телефона:")
+    await state.set_state(client_state.edit_client_tel)
+
+
+@router.message(client_state.edit_client_name)
+async def user_await(message: Message, state: FSMContext):
+    client_name = message.text
+    if len(client_name.split()) >= 2:
+        await state.update_data(client_name=client_name)
+        await state.set_state(client_state.user_await)
+        user_data = await state.get_data()
+        await message.answer(f"Ваши данные: \n "
+                             f"Имя: {user_data['client_name']}\n "
+                             f"Телефон: {user_data['client_tel']}",
+                             reply_markup=keyboards.get_client_tel_keyboard().as_markup())
+    else:
+        await message.answer(f"К сожалению, этих данных недостаточно( "
+                                      f"Проверьте наличие фамилии или имени и отправьте мне ФИО повторно:")
+        await state.set_state(client_state.edit_client_name)
+
+
+@router.message(client_state.edit_client_tel)
+async def user_await(message: Message, state: FSMContext):
+    client_tel = message.text
+    await state.update_data(client_tel=client_tel)
+    await state.set_state(client_state.user_await)
+    user_data = await state.get_data()
+    await message.answer(f"Ваши данные: \n "
+                         f"Имя: {user_data['client_name']}\n "
+                         f"Телефон: {user_data['client_tel']}",
+                         reply_markup=keyboards.get_client_tel_keyboard().as_markup())
+    await state.set_state(client_state.user_await)
+
+# SAVE USER
+@router.callback_query(F.data == "client_data_save")
+async def client_data_save_callback(callback: types.CallbackQuery, state: FSMContext):
+    user_data = await state.get_data()
+    user_manager = User()
+    await user_manager.create_user(callback.message.chat.id, user_data["client_name"], user_data["client_tel"])
+    await callback.message.edit_text(f"Данные сохранены. Меню ")
+    await state.clear()
--- handlers/keyboards.py
+++ handlers/keyboards.py
@@ -15,5 +15,6 @@ def client_data_edit_keyboard(name, tel):
     builder = InlineKeyboardBuilder()
     builder.row(types.InlineKeyboardButton(text=f"{name}", callback_data="client_data_edit_name"),
                 types.InlineKeyboardButton(text=f"{tel}", callback_data="client_data_edit_tel"),
+                types.InlineKeyboardButton(text=f"« Назад", callback_data="user_await"),
                 width=2)
     return builder
