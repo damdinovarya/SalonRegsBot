a5499c502d1d0ed83ea5e703777151992299d71f
Damdinovaria <addamdinov@edu.hse.ru>
2024-12-06 15:17:49 +0300

    yclients staff and services

---
+++ yclients/api_client.py
@@ -0,0 +1,29 @@
+# TOKEN = "b238a6679665f570af0f1a21ce183d7b"
+# CID = 1186779
+# FID = 1301768
+
+from yclients import YClientsAPI
+
+if __name__ == '__main__':
+
+    TOKEN = "nzdj6eabmyj9kd3mbmjk"
+    СID = 1186779
+    FID = 1301768
+
+    login = "hooooogrideeer@gmail.com"
+    password = "zh33ek"
+
+    # Create api object
+    api = YClientsAPI(token=TOKEN, company_id=СID, form_id=FID)
+    user_token = api.get_user_token(login, password)
+    api.update_user_token(user_token)
+
+    api.show_debugging()
+
+    clients_data_list = api.get_clients_data()
+    staff_data_list = api.get_staff()['data']
+    services_data_list = api.get_services()['data']['services']
+
+    print(clients_data_list)
+    print(staff_data_list)
+    print(services_data_list)
